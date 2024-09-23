9e7c34219020198ce8b0d82c167f9b31b3dbb8a0
Jack N <nystromjp@gmail.com>
2024-09-23 15:36:03 -0700

    add time since last commit :P

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,36 @@
-import { defineConfig } from 'astro/config';
-import gruvboxMaterialDark from './gruvbox_syntax_highlighting_theme.json';
+import { defineConfig } from "astro/config";
+import { execSync } from "child_process";
+import gruvboxMaterialDark from "./gruvbox_syntax_highlighting_theme.json";
 
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings/lib';
-import rehypeSlug from 'rehype-slug';
-import remarkToc from 'remark-toc';
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from "rehype-autolink-headings/lib";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        define: {
+            TIME_OF_LAST_COMMIT: (execSync("git log -1 --pretty=format:'%at'") * 1000).toString(),
+        },
+    },
     markdown: {
         shikiConfig: {
-            theme: gruvboxMaterialDark
+            theme: gruvboxMaterialDark,
         },
         rehypePlugins: [
-            [ rehypeExternalLinks, {target: "_blank", rel: "noopener noreferrer"} ],
-            [ rehypeSlug, {} ],
-            [ rehypeAutolinkHeadings, {behavior: "append"} ],
+            [
+                rehypeExternalLinks,
+                { target: "_blank", rel: "noopener noreferrer" },
+            ],
+            [rehypeSlug, {}],
+            [rehypeAutolinkHeadings, { behavior: "append" }],
             // 'rehype-toc'
         ],
         remarkPlugins: [
             // [ remarkToc, {heading: 'contents'} ],
             // 'remark-toc'
-        ]
-    }
+        ],
+    },
 });
+
