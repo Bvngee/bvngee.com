8ecb17d597bdc65d5bac1f8e7911d5984f77106b
Jack Nystrom <nystromjp@gmail.com>
2025-05-03 12:16:42 -0700

    Update get_language_color.sh

--- get_language_color.sh
+++ get_language_color.sh
@@ -5,7 +5,7 @@ lang="$1"
 color="$(curl -s https://raw.githubusercontent.com/github-linguist/linguist/master/lib/linguist/languages.yml | yq ".\"${lang}\".color")"
 
 if [[ $color == "null" ]]; then
-    echo "color not in languages.yml"
+    echo "lang not in languages.yml"
     exit 1
 fi
 
